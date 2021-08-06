3c22bc1b6d434b2ef46565e21f83bd3cc2258a24
Robinson Silva <robinson.silva@linkconsulting.com.br>
2021-08-06 14:04:05 -0300

    criado navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,62 @@
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  makeStyles,
+  AppBar,
+  Toolbar,
+  SvgIcon,
+  Avatar,
+} from "@material-ui/core";
+import { Bell } from "react-feather";
 
-function Header(){
-    return (        <header className='header'>
-    <div className='toolbar'>
-        <div className=''>
-            <span>curso react</span>
+const useStyles = makeStyles({
+  root: {},
+  appBar: {
+    boxShadow: "none",
+  },
+  img: {
+    maxHeight: 55,
+  },
+  grow: {
+      flexGrow: 1
+  },
+  userSection: {
+      display: 'flex',
+      alignItems: 'center'
+  },
+  button: {
+      marginRight: 10
+  },
+  bell: {
+      marginRight: 10
+  }
+});
+function Header() {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+      <Toolbar>
+        <img src="/images/logo.png" alt="logo" className={classes.img}></img>
+        <div className={classes.grow}></div>
+        <div className={classes.userSection}>
+          <Button color="primary" variant="contained" className={classes.button}>
+            Novo Post
+          </Button>
+          <SvgIcon className={classes.bell}>
+            <Bell></Bell>
+          </SvgIcon>
+          <Avatar alt="Avatar" src="/"></Avatar>
         </div>
-        <div className=''>
-            <Button color="primary" variant="contained">Novo Post</Button>
-            <span>img1</span>
-            <span>img2</span>
+        {/* <div className="">
+          <span>curso react</span>
         </div>
-    </div>
-</header>);
+        <div className="">
+
+          <span>img1</span>
+          <span>img2</span>
+        </div> */}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
